6de8d950e10806ef319c620be8d5883741c22964
snehayadavv <risesnrise@gmail.com>
2018-06-17 11:19:23 +0530

    modified

--- colourGame.js
+++ colourGame.js
@@ -7,7 +7,25 @@ var colors = [
 "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor=colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message")
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
-  squares[i].style.backgroundColor = colors[i];
+
+  squares[i].style.background = colors[i];
+
+  squares[i].addEventListener("click", function(){
+    //alert("clicked a square");
+  var clickedColor = this.style.background;
+
+  if(clickedColor === pickedColor){
+    alert("Correct!");
+  }  else{
+      this.style.background = "#232323";
+      messageDisplay.textContent="try again";
+    }
+  });
 }
