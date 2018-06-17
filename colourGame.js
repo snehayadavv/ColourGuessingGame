11629efae149435170366020e042b4debb51844d
snehayadavv <risesnrise@gmail.com>
2018-06-17 12:47:28 +0530

    modified

--- colourGame.js
+++ colourGame.js
@@ -6,8 +6,9 @@ var colors = [
 "rgb(0, 0, 255)",
 "rgb(255, 0, 255)"
 ]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor=colors[3];
+var pickedColor=pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message")
 
@@ -22,10 +23,36 @@ for (var i = 0; i < squares.length; i++) {
   var clickedColor = this.style.background;
 
   if(clickedColor === pickedColor){
-    alert("Correct!");
+    messageDisplay.textContent = "correct";
+    changeColors(clickedColor);
   }  else{
       this.style.background = "#232323";
       messageDisplay.textContent="try again";
     }
   });
+}
+
+function changeColors(color){
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+
+  }
+}
+function  pickColor(){
+  var random=Math.floor(Math.random()*colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num){
+  var arr = []
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+  return arr;
+}
+function randomColor(){
+  var r= Math.floor(Math.random()*256);
+  var g= Math.floor(Math.random()*256);
+  var b= Math.floor(Math.random()*256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
